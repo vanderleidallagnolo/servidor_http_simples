04b2a74934484eb8295434c06c4fc9c8b047000a
vanderleidallagnolo <vanderleidallagnolo@gmail.com>
2022-04-03 19:56:05 -0300

    Fixa index.html como valor default da requisição

--- ServidorHttp.cs
+++ ServidorHttp.cs
@@ -131,6 +131,9 @@ class ServidorHttp
                 int iSegundoEspaco = linhas[0].LastIndexOf(' ');
                 string metodoHttp = linhas[0].Substring(0, iPrimeiroEspaco);
                 string recursoBuscado = linhas[0].Substring(iPrimeiroEspaco + 1, iSegundoEspaco - iPrimeiroEspaco - 1);
+                
+                if (recursoBuscado == "/") recursoBuscado = "/index.html";
+                
                 string versaoHttp = linhas[0].Substring(iSegundoEspaco + 1);
 
                 iPrimeiroEspaco = linhas[1].IndexOf(' ');
