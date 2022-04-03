966b62a61a1d3af25f9270432342ccfeab04eada
vanderleidallagnolo <vanderleidallagnolo@gmail.com>
2022-04-03 08:22:45 -0300

    Aplicação criada
    O modelo 'Console Application' foi criado com êxito.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace simplehttpserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
